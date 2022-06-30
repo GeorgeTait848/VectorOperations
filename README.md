66e137fc14a64c23499c8c0ec1fadd52a1e04920
George Tait <george.tait512@gmail.com>
2022-06-30 09:56:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# VectorOperations
+
+This package brings elementwise numerical operations to arrays. 
+
+Arrays have extended functionality to elementwise 
+add, subtract, multiply and divide with  Doubles, Ints, or 
+other Arrays.  
---
+++ Tests/VectorOperationsTests/VectorOperationsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VectorOperations
+
+final class VectorOperationsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VectorOperations().text, "Hello, World!")
+    }
+}
